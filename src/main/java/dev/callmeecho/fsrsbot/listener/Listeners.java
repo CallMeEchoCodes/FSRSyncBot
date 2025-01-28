f913c73f583fed18e354caf9e5981c99a24f39fb
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2025-01-28 15:05:08 +1100

    fix: fix the fix

--- src/main/java/dev/callmeecho/fsrsbot/listener/Listeners.java
+++ src/main/java/dev/callmeecho/fsrsbot/listener/Listeners.java
@@ -10,6 +10,9 @@ public final class Listeners {
 
 	public static final RoleSyncRemoveListener ROLE_SYNC_REMOVE = register("role_sync_remove", new RoleSyncRemoveListener());
 
+	public static final MemberJoinListener MEMBER_JOIN = register("member_join", new MemberJoinListener());
+
+	public static final MemberLeaveListener MEMBER_LEAVE = register("member_leave", new MemberLeaveListener());
 
 	private static <T extends GloballyAttachableListener> T register(String id, T entry) {
 		Registries.LISTENER.register(id, entry);
